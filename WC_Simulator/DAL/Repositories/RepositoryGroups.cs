da7e780d2c0616c6e64e634306a38a110e6e1c6e
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2022-06-08 10:03:42 +0200

    Added queries to RepositoryGroups

--- WC_Simulator/DAL/Repositories/RepositoryGroups.cs
+++ WC_Simulator/DAL/Repositories/RepositoryGroups.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace WC_Simulator.DAL.Repositories
 {
+    using WC_Simulator.DAL;
     class RepositoryGroups
     {
+        #region QUERIES
+        private const string ALL_GROUP = "SELECT * FROM `single_group`";
+        private const string ADD_GROUP = "INSERT INTO `single_group`(`id_group`, `id_first_pl_team`, `id_second_pl_team`, `id_tournament`, `letter`) VALUES ";
+        private const string DELETE_GROUP = "DELETE FROM `single_group` WHERE id_group = ";
+        //private const string UPDATE_GROUP = "UPDATE `single_group` SET xx WHERE id_group = ";
+        #endregion
+
+        #region CRUD
+
+        #endregion
     }
 }
