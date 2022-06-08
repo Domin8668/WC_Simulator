dada3c67650b0a428a5cddd5327af8ff4d035833
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-06-08 23:59:46 +0200

    Added draft LoginViewModel

--- WC_Simulator/ViewModel/LoginViewModel.cs
+++ WC_Simulator/ViewModel/LoginViewModel.cs
@@ -1,9 +1,29 @@
-﻿using WC_Simulator.ViewModel.BaseClasses;
+﻿using WC_Simulator.DAL.Entities;
+using WC_Simulator.ViewModel.BaseClasses;
 
 namespace WC_Simulator.ViewModel
 {
     internal class LoginViewModel : BaseViewModel
     {
+        #region Variables
+        #endregion
 
+        #region Properties
+
+        private User _currentUser;
+
+        public User CurrentUser
+        {
+            get { return _currentUser; }
+            set { _currentUser = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Dependencies
+        #endregion
     }
 }
