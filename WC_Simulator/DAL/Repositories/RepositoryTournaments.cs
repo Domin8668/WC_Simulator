f7c0d432802899fe982ccfb052c446b07ec12fa8
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2022-06-09 15:28:28 +0200

    Added queries to RepositoryTournaments and Users

--- WC_Simulator/DAL/Repositories/RepositoryTournaments.cs
+++ WC_Simulator/DAL/Repositories/RepositoryTournaments.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace WC_Simulator.DAL.Repositories
 {
+    using WC_Simulator.DAL;
     class RepositoryTournaments
     {
+        #region QUERIES
+        private const string ALL_TOURNAMENT = "SELECT * FROM `tournament`";
+        private const string ADD_TOURNAMENT = "INSERT INTO `tournament`(`id_tournament`, `id_user`) VALUES ";
+        private const string DELETE_TOURNAMENT = "DELETE FROM `tournament` WHERE id_tournament = ";
+        //private const string UPDATE_TOURNAMENT = "UPDATE `tournament` SET xx WHERE id_tournament = ";
+        #endregion
+
+        #region CRUD
+
+        #endregion
     }
 }
--- WC_Simulator/DAL/Repositories/RepositoryUsers.cs
+++ WC_Simulator/DAL/Repositories/RepositoryUsers.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace WC_Simulator.DAL.Repositories
 {
+    using WC_Simulator.DAL;
     class RepositoryUsers
     {
+        #region QUERIES
+        private const string ALL_USER = "SELECT * FROM `user`";
+        private const string ADD_USER = "INSERT INTO `user`(`id_user`, `login`, `password`," +
+            " `creation_date`, `last_log_date`, `security_question`, `security_answer`) VALUES ";
+        private const string DELETE_USER = "DELETE FROM `user` WHERE id_user = ";
+        //private const string UPDATE_USER = "UPDATE `user` SET xx WHERE id_user = ";
+        #endregion
+
+        #region CRUD
+
+        #endregion
     }
 }
