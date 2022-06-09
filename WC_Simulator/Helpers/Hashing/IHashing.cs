62c104d3cf1d5e43bca7bff7fb83ac405893493f
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-06-09 13:51:03 +0200

    Added hashing interface and  and a class implementing SHA256

---
+++ WC_Simulator/Helpers/Hashing/IHashing.cs
@@ -0,0 +1,9 @@
+﻿namespace WC_Simulator.Helpers.Hashing
+{
+    internal interface IHashing
+    {
+        bool MatchHashes(byte[] hash1, byte[] hash2);
+
+        byte[] GetHash(string userID, string password);
+    }
+}
---
+++ WC_Simulator/Helpers/Hashing/SHA256Hashing.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WC_Simulator.Helpers.Hashing
+{
+    internal class SHA256Hashing : IHashing
+    {
+        public bool MatchHashes(byte[] hash1, byte[] hash2)
+        {
+            bool result = false;
+            if (hash1 != null && hash2 != null)
+            {
+                if (hash1.Length == hash2.Length)
+                {
+                    result = true;
+                    for (int i = 0; i < hash1.Length; i++)
+                    {
+                        if (hash1[i] != hash2[i])
+                        {
+                            result = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public byte[] GetHash(string userID, string password)
+        {
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                return mySHA256.ComputeHash(Encoding.ASCII.GetBytes(userID + password));
+            }
+    }
+}
