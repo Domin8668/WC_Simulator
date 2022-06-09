df1799d9a12c0562395619f546c0a7f228acfa2a
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-06-09 20:26:10 +0200

    Added Match view and viewmodel to test viewswitching

---
+++ WC_Simulator/ViewModel/MatchViewModel.cs
@@ -0,0 +1,34 @@
+﻿using WC_Simulator.Model;
+
+namespace WC_Simulator.ViewModel
+{
+    internal class MatchViewModel
+    {
+        #region Variables
+
+        private MainModel _model;
+
+        #endregion
+
+
+        #region Constructor
+
+        public MatchViewModel(MainModel Model)
+        {
+            this.Model = Model;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public MainModel Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        #endregion
+    }
+}
