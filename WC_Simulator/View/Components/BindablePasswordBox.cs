c0d82ac52c41cc8b05ab677e0ddbd68b42f14ee9
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-06-10 21:45:08 +0200

    Added a bindable password box decorator and a working login user control

---
+++ WC_Simulator/View/Components/BindablePasswordBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WC_Simulator.View.Components
+{
+    public class BindablePasswordBox : Decorator
+    {
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox), new PropertyMetadata(string.Empty, OnDependencyPropertyChanged));
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        private static void OnDependencyPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            BindablePasswordBox p = source as BindablePasswordBox;
+            if (p != null)
+            {
+                if (e.Property == PasswordProperty)
+                {
+                    var pb = p.Child as PasswordBox;
+                    if (pb != null)
+                    {
+                        if (pb.Password != p.Password)
+                            pb.Password = p.Password;
+                    }
+                }
+            }
+        }
+
+        public BindablePasswordBox()
+        {
+            Child = new PasswordBox();
+            ((PasswordBox)Child).PasswordChanged += BindablePasswordBox_PasswordChanged;
+        }
+
+        void BindablePasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = ((PasswordBox)Child).Password;
+        }
+    }
+}
