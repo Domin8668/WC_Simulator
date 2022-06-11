03f79339c0aa403a8c6df733d3a4c8d0d27da702
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-06-11 20:22:10 +0200

    Commented out dependecies and properties

--- WC_Simulator/View/ProfileView.xaml.cs
+++ WC_Simulator/View/ProfileView.xaml.cs
@@ -25,81 +25,81 @@ namespace WC_Simulator.View
             InitializeComponent();
         }
 
-        #region Dependencies
+        //#region Dependencies
 
-        public static readonly DependencyProperty LoginProperty = DependencyProperty.Register(
-            "Login",
-            typeof(string),
-            typeof(ProfileView),
-            new FrameworkPropertyMetadata(null)
-            );
+        //public static readonly DependencyProperty LoginProperty = DependencyProperty.Register(
+        //    "Login",
+        //    typeof(string),
+        //    typeof(ProfileView),
+        //    new FrameworkPropertyMetadata(null)
+        //    );
 
-        public static readonly DependencyProperty TourneyCountProperty = DependencyProperty.Register(
-            "TourneyCount",
-            typeof(int),
-            typeof(ProfileView),
-            new FrameworkPropertyMetadata(null)
-            );
+        //public static readonly DependencyProperty TourneyCountProperty = DependencyProperty.Register(
+        //    "TourneyCount",
+        //    typeof(int),
+        //    typeof(ProfileView),
+        //    new FrameworkPropertyMetadata(null)
+        //    );
 
-        public static readonly DependencyProperty FirstDateProperty = DependencyProperty.Register(
-            "FirstDate",
-            typeof(string),
-            typeof(ProfileView),
-            new FrameworkPropertyMetadata(null)
-            );
+        //public static readonly DependencyProperty FirstDateProperty = DependencyProperty.Register(
+        //    "FirstDate",
+        //    typeof(string),
+        //    typeof(ProfileView),
+        //    new FrameworkPropertyMetadata(null)
+        //    );
 
-        public static readonly DependencyProperty LastDateProperty = DependencyProperty.Register(
-            "LastDate",
-            typeof(string),
-            typeof(ProfileView),
-            new FrameworkPropertyMetadata(null)
-            );
+        //public static readonly DependencyProperty LastDateProperty = DependencyProperty.Register(
+        //    "LastDate",
+        //    typeof(string),
+        //    typeof(ProfileView),
+        //    new FrameworkPropertyMetadata(null)
+        //    );
 
-        public static readonly DependencyProperty TimeInServiceProperty = DependencyProperty.Register(
-            "TimeInService",
-            typeof(string),
-            typeof(ProfileView),
-            new FrameworkPropertyMetadata(null)
-            );
-
-
-        #endregion
-
-        #region Properties
-
-        public string Login
-        {
-            get { return (string)GetValue(LoginProperty); }
-            set { SetValue(LoginProperty, value); }
-        }
-
-        public int TourneyCount
-        {
-            get { return (int)GetValue(TourneyCountProperty); }
-            set { SetValue(TourneyCountProperty, value); }
-        }
+        //public static readonly DependencyProperty TimeInServiceProperty = DependencyProperty.Register(
+        //    "TimeInService",
+        //    typeof(string),
+        //    typeof(ProfileView),
+        //    new FrameworkPropertyMetadata(null)
+        //    );
 
 
-        public string FirstDate
-        {
-            get { return (string)GetValue(FirstDateProperty); }
-            set { SetValue(FirstDateProperty, value); }
-        }
+        //#endregion
 
-        public string LastDate
-        {
-            get { return (string)GetValue(LastDateProperty); }
-            set { SetValue(LastDateProperty, value); }
-        }
+        //#region Properties
 
-        public string TimeInService
-        {
-            get { return (string)GetValue(TimeInServiceProperty); }
-            set { SetValue(TimeInServiceProperty, value); }
-        }
+        //public string Login
+        //{
+        //    get { return (string)GetValue(LoginProperty); }
+        //    set { SetValue(LoginProperty, value); }
+        //}
+
+        //public int TourneyCount
+        //{
+        //    get { return (int)GetValue(TourneyCountProperty); }
+        //    set { SetValue(TourneyCountProperty, value); }
+        //}
 
 
-        #endregion
+        //public string FirstDate
+        //{
+        //    get { return (string)GetValue(FirstDateProperty); }
+        //    set { SetValue(FirstDateProperty, value); }
+        //}
+
+        //public string LastDate
+        //{
+        //    get { return (string)GetValue(LastDateProperty); }
+        //    set { SetValue(LastDateProperty, value); }
+        //}
+
+        //public string TimeInService
+        //{
+        //    get { return (string)GetValue(TimeInServiceProperty); }
+        //    set { SetValue(TimeInServiceProperty, value); }
+        //}
+
+
+        //#endregion
 
     }
 }
