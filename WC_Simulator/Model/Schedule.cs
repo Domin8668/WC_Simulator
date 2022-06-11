47eaf73bf416d880b56b6856658585333baa0a50
Rolero4 <72434958+Rolero4@users.noreply.github.com>
2022-06-11 14:55:08 +0200

    Poprawka Schedule i ScheduleMatch

--- WC_Simulator/Model/Schedule.cs
+++ WC_Simulator/Model/Schedule.cs
@@ -9,19 +9,47 @@ namespace WC_Simulator.Model
      class Schedule
     {
         #region Variables
-        public int[] match_codes = new int[64];
-        public int[] match_stages = new int[64];
-        public int[] match_locations = new int[64];
-        public int[] match_referees = new int[64];
-        public DateTime[] match_dates = new DateTime[64];
-        public ScheduleMatch[] schedule = new ScheduleMatch[64];
+        private int[] _matchCodes = new int[64];
+        private int[] _matchStages = new int[64];
+        private int[] _matchLocations = new int[64];
+        private int[] _matchReferees = new int[64];
+        private DateTime[] _matchDates = new DateTime[64];
+        private ScheduleMatch[] _finalSchedule = new ScheduleMatch[64];
         #endregion
-
 
         #region Properties
-        
-        #endregion
+        public int[] MatchCodes
+        {
+            get { return _matchCodes; }
+            set { _matchCodes = value; }
+        }
+        public int[] MatchStages
+        {
+            get { return _matchStages; }
+            set { _matchStages = value; }
+        }
+        public int[] MatchLocations
+        {
+            get { return _matchLocations; }
+            set { _matchLocations = value; }
+        }
+        public int[] MatchReferees
+        {
+            get { return _matchReferees; }
+            set { _matchReferees = value; }
+        }
+        public DateTime[] MatchDates
+        {
+            get { return _matchDates; }
+            set { _matchDates = value; }
+        }
+        public ScheduleMatch[] FinalSchedule
+        {
+            get { return _finalSchedule; }
+            set { _finalSchedule = value; }
+        }
 
+        #endregion
 
         #region Constructor
         public Schedule()
@@ -37,28 +65,27 @@ namespace WC_Simulator.Model
 
             for (int i = 0; i < 40; i++)
             {
-                november[i] = new ScheduleMatch(match_codes[i], match_stages[i], match_locations[i], match_referees[i], match_dates[i]);
+                november[i] = new ScheduleMatch(_matchCodes[i], _matchStages[i], _matchLocations[i], _matchReferees[i], MatchDates[i]);
             }
-            var novqry = november.Select(p => p).OrderBy(p => p.Date).ThenBy(p => p.Time).ThenBy(p => p.Match_code);
+            var novqry = november.Select(p => p).OrderBy(p => p.DateTimeVariable).ThenBy(p => p.MatchCode);
 
 
             for (int i = 0; i < 24; i++)
             {
-                december[i] = new ScheduleMatch(match_codes[40 + i], match_stages[40 + i], match_locations[40 + i], match_referees[40 + i], match_dates[40 + i]);
+                december[i] = new ScheduleMatch(_matchCodes[40 + i], _matchStages[40 + i], _matchLocations[40 + i], _matchReferees[40 + i], MatchDates[40 + i]);
             }
-            var decqry = december.Select(p => p).OrderBy(p => p.Date).ThenBy(p => p.Time).ThenBy(p => p.Match_code);
+            var decqry = december.Select(p => p).OrderBy(p => p.DateTimeVariable).ThenBy(p => p.MatchCode);
 
-            schedule = novqry.Concat(decqry).ToArray();
+            FinalSchedule = novqry.Concat(decqry).ToArray();
         }
         #endregion
-
 
         #region Methods
         private void Prepare_matchcodes()
         {
             for (int i = 0; i < 64; i++)
             {
-                match_codes[i] = i;
+                MatchCodes[i] = i;
             }
         }
         private void Prepare_matchstages()
@@ -66,26 +93,26 @@ namespace WC_Simulator.Model
             // group
             for (int i = 0; i < 48; i++)
             {
-                match_stages[i] = 0;
+                MatchStages[i] = 0;
             }
             // 1/8
             for (int i = 48; i < 56; i++)
             {
-                match_stages[i] = 1;
+                MatchStages[i] = 1;
             }
             // 1/4
             for (int i = 56; i < 60; i++)
             {
-                match_stages[i] = 2;
+                MatchStages[i] = 2;
             }
             // 1/2
             for (int i = 60; i < 62; i++)
             {
-                match_stages[i] = 3;
+                MatchStages[i] = 3;
             }
             // 3rd and final
-            match_stages[62] = 4;
-            match_stages[63] = 5;
+            MatchStages[62] = 4;
+            MatchStages[63] = 5;
         }
         private void Prepare_matchlocations()
         {
@@ -93,49 +120,49 @@ namespace WC_Simulator.Model
             int[] al_bayt = new int[9] { 0, 11, 19, 27, 35,43,50,58, 61}; // 0
             foreach (int i in al_bayt)
             {
-                match_locations[i] = 0;
+                MatchLocations[i] = 0;
             }
 
             int[] lusail = new int[10] { 7, 15, 23, 31, 39, 47, 55, 56, 60, 63 }; // 1
             foreach (int i in lusail)
             {
-                match_locations[i] = 1;
+                MatchLocations[i] = 1;
             }
 
             int[] stadium_974 = new int[7] { 6, 14, 22, 30, 38, 46, 53 }; // 2
             foreach (int i in stadium_974)
             {
-                match_locations[i] = 2;
+                MatchLocations[i] = 2;
             }
 
             int[] al_thumana = new int[8] { 1, 9, 17, 25, 33, 41, 51, 59 }; //3
             foreach (int i in al_thumana)
             {
-                match_locations[i] = 3;
+                MatchLocations[i] = 3;
             }
 
             int[] education = new int[8] { 5, 13, 21, 29, 37, 45, 54, 57}; //4
             foreach (int i in education)
             {
-                match_locations[i] = 4;
+                MatchLocations[i] = 4;
             }
 
             int[] ahmad = new int[7] { 3, 8, 16, 24, 32, 40, 49 }; //5
             foreach (int i in ahmad)
             {
-                match_locations[i] = 5;
+                MatchLocations[i] = 5;
             }
 
             int[] khalifa_inter = new int[8] { 2, 10, 18, 26, 34, 42, 48, 62}; //6
             foreach (int i in khalifa_inter)
             {
-                match_locations[i] = 6;
+                MatchLocations[i] = 6;
             }
 
             int[] al_janoub = new int[7] { 4, 12, 20, 28, 36, 44, 52 }; //7
             foreach (int i in al_janoub)
             {
-                match_locations[i] = 7;
+                MatchLocations[i] = 7;
             }
         }
         private void Prepare_matchdates()
@@ -152,66 +179,66 @@ namespace WC_Simulator.Model
             int day = 21;
             foreach(int element in eleven)
             {
-                match_dates[element] = new DateTime(2022, 11, day, 11,0,0);
+                MatchDates[element] = new DateTime(2022, 11, day, 11,0,0);
                 day++;
             }
             // 14
             day = 21;
             foreach (int element in fourteen)
             {
-                match_dates[element] = new DateTime(2022, 11, day, 14,0,0);
+                MatchDates[element] = new DateTime(2022, 11, day, 14,0,0);
                 day++;
             }
             // 17
             day = 21;
             foreach (var element in seventeen)
             {
-                match_dates[element] = new DateTime(2022, 11, day,17,0,0);
+                MatchDates[element] = new DateTime(2022, 11, day,17,0,0);
                 day++;
             }
             // 20
             for (int q = 0; q < 8; q++)
             {
-                match_dates[twenty[q]] = new DateTime(2022, 11, 21 + q, 20, 0, 0);
+                MatchDates[twenty[q]] = new DateTime(2022, 11, 21 + q, 20, 0, 0);
             }
 
             #endregion
 
             #region ostatnia kolejka
-            match_dates[32] = new DateTime(2022, 11, 29, 20, 0, 0);
-            match_dates[33] = new DateTime(2022, 11, 29, 20, 0, 0);
-            match_dates[34] = new DateTime(2022, 11, 29, 16, 0, 0);
-            match_dates[35] = new DateTime(2022, 11, 29, 16, 0, 0);
+            MatchDates[32] = new DateTime(2022, 11, 29, 20, 0, 0);
+            MatchDates[33] = new DateTime(2022, 11, 29, 20, 0, 0);
+            MatchDates[34] = new DateTime(2022, 11, 29, 16, 0, 0);
+            MatchDates[35] = new DateTime(2022, 11, 29, 16, 0, 0);
 
-            match_dates[36] = new DateTime(2022, 11, 30, 16, 0, 0);
-            match_dates[37] = new DateTime(2022, 11, 30, 16, 0, 0);
-            match_dates[38] = new DateTime(2022, 11, 30, 20, 0, 0);
-            match_dates[39] = new DateTime(2022, 11, 30, 20, 0, 0);
+            MatchDates[36] = new DateTime(2022, 11, 30, 16, 0, 0);
+            MatchDates[37] = new DateTime(2022, 11, 30, 16, 0, 0);
+            MatchDates[38] = new DateTime(2022, 11, 30, 20, 0, 0);
+            MatchDates[39] = new DateTime(2022, 11, 30, 20, 0, 0);
 
-            match_dates[40] = new DateTime(2022, 12, 1, 16, 0, 0);
-            match_dates[41] = new DateTime(2022, 12, 1, 16, 0, 0);
-            match_dates[42] = new DateTime(2022, 12, 1, 20, 0, 0);
-            match_dates[43] = new DateTime(2022, 12, 1, 20, 0, 0);
+            MatchDates[40] = new DateTime(2022, 12, 1, 16, 0, 0);
+            MatchDates[41] = new DateTime(2022, 12, 1, 16, 0, 0);
+            MatchDates[42] = new DateTime(2022, 12, 1, 20, 0, 0);
+            MatchDates[43] = new DateTime(2022, 12, 1, 20, 0, 0);
 
-            match_dates[44] = new DateTime(2022, 12, 2, 16, 0, 0);
-            match_dates[45] = new DateTime(2022, 12, 2, 16, 0, 0);
-            match_dates[46] = new DateTime(2022, 12, 2, 20, 0, 0);
-            match_dates[47] = new DateTime(2022, 12, 2, 20, 0, 0);
+            MatchDates[44] = new DateTime(2022, 12, 2, 16, 0, 0);
+            MatchDates[45] = new DateTime(2022, 12, 2, 16, 0, 0);
+            MatchDates[46] = new DateTime(2022, 12, 2, 20, 0, 0);
+            MatchDates[47] = new DateTime(2022, 12, 2, 20, 0, 0);
             #endregion
 
             #region 1/8
             // pierwsza połówka
-            match_dates[48] = new DateTime(2022, 12, 3, 16, 0, 0);
-            match_dates[49] = new DateTime(2022, 12, 3, 20, 0, 0);
-            match_dates[50] = new DateTime(2022, 12, 4, 20, 0, 0);
-            match_dates[51] = new DateTime(2022, 12, 4, 16, 0, 0);
+            MatchDates[48] = new DateTime(2022, 12, 3, 16, 0, 0);
+            MatchDates[49] = new DateTime(2022, 12, 3, 20, 0, 0);
+            MatchDates[50] = new DateTime(2022, 12, 4, 20, 0, 0);
+            MatchDates[51] = new DateTime(2022, 12, 4, 16, 0, 0);
 
             // druga połówka
             day = 5;
             for (int i = 52; i < 56; i += 2)
             {
-                match_dates[i] = new DateTime(2022, 12, day, 16,0,0);
-                match_dates[i + 1] = new DateTime(2022, 12, day, 20, 0, 0);
+                MatchDates[i] = new DateTime(2022, 12, day, 16,0,0);
+                MatchDates[i + 1] = new DateTime(2022, 12, day, 20, 0, 0);
                 day++;
             }
 
@@ -222,8 +249,8 @@ namespace WC_Simulator.Model
             int hour = 20;
             for (int i = 56; i < 60; i++)
             {
-                match_dates[i] = new DateTime(2022, 12, day, hour, 0, 0);
-                match_dates[i + 2] = new DateTime(2022, 12, day+1, hour, 0, 0);
+                MatchDates[i] = new DateTime(2022, 12, day, hour, 0, 0);
+                MatchDates[i + 2] = new DateTime(2022, 12, day+1, hour, 0, 0);
                 hour = 16;  
             }
             #endregion
@@ -231,11 +258,11 @@ namespace WC_Simulator.Model
             #region strefa finałowa
             for (int i = 8; i <10; i++)
             {
-                match_dates[twenty[i]] = new DateTime(2022, 12, 5 + i, 20, 0, 0);
+                MatchDates[twenty[i]] = new DateTime(2022, 12, 5 + i, 20, 0, 0);
             }
             for (int i = 6; i < 8; i++)
             {
-                match_dates[sixteen[i]] = new DateTime(2022, 12, 11 + i, 16, 0, 0);
+                MatchDates[sixteen[i]] = new DateTime(2022, 12, 11 + i, 16, 0, 0);
             }
             #endregion
         }
@@ -245,16 +272,15 @@ namespace WC_Simulator.Model
             {
                 if (i < 35)
                 {
-                    match_referees[i] = i;
+                    MatchReferees[i] = i;
                 }
                 else
                 {
-                    match_referees[i] = i - 35;
+                    MatchReferees[i] = i - 35;
                 }
             }
-            match_referees[63] = 0;
+            MatchReferees[63] = 0;
         }
         #endregion
-
     }
 }
