90c62954c7f227dd634e594d077c2b18a7ed222b
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2022-06-11 09:13:07 +0200

    Updated MatchUserControl

--- WC_Simulator/View/Components/MatchUserControl.xaml.cs
+++ WC_Simulator/View/Components/MatchUserControl.xaml.cs
@@ -26,88 +26,93 @@ namespace WC_Simulator.View.Components
         }
 
         #region Dependencies
+
         // TEAM 1
-        public static readonly DependencyProperty Team2_Flag_PathProperty =
+        public static readonly DependencyProperty Team1FlagPathProperty =
             DependencyProperty.Register(
-                "Team2_Flag_Path",                      // nazwa właściwości
+                "Team1FlagPath",                      // nazwa właściwości
                 typeof(string),                     // typ właściwości
                 typeof(MatchUserControl),                    // typ właściciela (czyli tutaj własna kontrolka)
                 new FrameworkPropertyMetadata(null) // dodatkowe info
             );
 
-        public string Team2_Flag_Path
-        {
-            get { return (string)GetValue(Team2_Flag_PathProperty); }
-            set { SetValue(Team2_Flag_PathProperty, value); }
-        }
-
-
-        public static readonly DependencyProperty Team2_ShortNameProperty = DependencyProperty.Register(
-                "Team2_ShortName",
+        public static readonly DependencyProperty Team1ShortNameProperty = DependencyProperty.Register(
+                "Team1ShortName",
                 typeof(string),
                 typeof(MatchUserControl),
                 new FrameworkPropertyMetadata(null)
             );
 
-        public string Team2_ShortName
-        {
-            get { return (string)GetValue(Team2_ShortNameProperty); }
-            set { SetValue(Team2_ShortNameProperty, value); }
-        }
-
-
-        public static readonly DependencyProperty Team2_ResultProperty = DependencyProperty.Register(
-                "Team2_Result",
+        public static readonly DependencyProperty Team1ResultProperty = DependencyProperty.Register(
+                "Team1Result",
                 typeof(string),
                 typeof(MatchUserControl),
                 new FrameworkPropertyMetadata(null)
             );
-        public string Team2_Result
+
+        // TEAM 2
+        public static readonly DependencyProperty Team2FlagPathProperty =
+            DependencyProperty.Register(
+                "Team2FlagPath",                      // nazwa właściwości
+                typeof(string),                     // typ właściwości
+                typeof(MatchUserControl),                    // typ właściciela (czyli tutaj własna kontrolka)
+                new FrameworkPropertyMetadata(null) // dodatkowe info
+            );
+
+        public static readonly DependencyProperty Team2ShortNameProperty = DependencyProperty.Register(
+                "Team2ShortName",
+                typeof(string),
+                typeof(MatchUserControl),
+                new FrameworkPropertyMetadata(null)
+            );
+
+        public static readonly DependencyProperty Team2ResultProperty = DependencyProperty.Register(
+                "Team2Result",
+                typeof(string),
+                typeof(MatchUserControl),
+                new FrameworkPropertyMetadata(null)
+            );
+
+        #endregion
+
+        #region Properties
+
+        // TEAM 1
+        public string Team1FlagPath
         {
-            get { return (string)GetValue(Team2_ResultProperty); }
-            set { SetValue(Team2_ResultProperty, value); }
+            get { return (string)GetValue(Team1FlagPathProperty); }
+            set { SetValue(Team1FlagPathProperty, value); }
+        }
+
+        public string Team1ShortName
+        {
+            get { return (string)GetValue(Team1ShortNameProperty); }
+            set { SetValue(Team1ShortNameProperty, value); }
+        }
+
+        public string Team1Result
+        {
+            get { return (string)GetValue(Team1ResultProperty); }
+            set { SetValue(Team1ResultProperty, value); }
         }
 
         // TEAM 2
-        public static readonly DependencyProperty Team1_Flag_PathProperty =
-            DependencyProperty.Register(
-                "Team1_Flag_Path",                      // nazwa właściwości
-                typeof(string),                     // typ właściwości
-                typeof(MatchUserControl),                    // typ właściciela (czyli tutaj własna kontrolka)
-                new FrameworkPropertyMetadata(null) // dodatkowe info
-            );
-
-        public string Team1_Flag_Path
+        public string Team2FlagPath
         {
-            get { return (string)GetValue(Team1_Flag_PathProperty); }
-            set { SetValue(Team1_Flag_PathProperty, value); }
+            get { return (string)GetValue(Team2FlagPathProperty); }
+            set { SetValue(Team2FlagPathProperty, value); }
         }
 
-
-        public static readonly DependencyProperty Team1_ShortNameProperty = DependencyProperty.Register(
-                "Team1_ShortName",
-                typeof(string),
-                typeof(MatchUserControl),
-                new FrameworkPropertyMetadata(null)
-            );
-
-        public string Team1_ShortName
+        public string Team2ShortName
         {
-            get { return (string)GetValue(Team1_ShortNameProperty); }
-            set { SetValue(Team1_ShortNameProperty, value); }
+            get { return (string)GetValue(Team2ShortNameProperty); }
+            set { SetValue(Team2ShortNameProperty, value); }
         }
 
-
-        public static readonly DependencyProperty Team1_ResultProperty = DependencyProperty.Register(
-                "Team1_Result",
-                typeof(string),
-                typeof(MatchUserControl),
-                new FrameworkPropertyMetadata(null)
-            );
-        public string Team1_Result
+        public string Team2Result
         {
-            get { return (string)GetValue(Team1_ResultProperty); }
-            set { SetValue(Team1_ResultProperty, value); }
+            get { return (string)GetValue(Team2ResultProperty); }
+            set { SetValue(Team2ResultProperty, value); }
         }
 
         #endregion
--- WC_Simulator/ViewModel/MatchViewModel.cs
+++ WC_Simulator/ViewModel/MatchViewModel.cs
@@ -7,9 +7,13 @@ namespace WC_Simulator.ViewModel
         #region Variables
 
         private MainModel _model;
-        private string _flag_img;
-        private string _team_short_name;
-        private string _result;
+        private string _t1FlagImg;
+        private string _t1ShortName;
+        private string _t1Result;
+
+        private string _t2FlagImg;
+        private string _t2ShortName;
+        private string _t2Result;
 
         #endregion
 
@@ -18,36 +22,62 @@ namespace WC_Simulator.ViewModel
 
         public MatchViewModel(MainModel model)
         {
-            _model = model;
+            Model = model;
         }
 
-        public MatchViewModel(string flag, string team, string res)
+        public MatchViewModel(string flag1, string team1, string res1, string flag2, string team2, string res2)
         {
-            _flag_img = $"../../Resources/Flags/{flag.ToLower()}";
-            _team_short_name = team;
-            _result = res;
+            T1FlagImg = $"../../Resources/Flags/{flag1.ToLower()}";
+            T1ShortName = team1;
+            T1Result = res1;
+            T2FlagImg = $"../../Resources/Flags/{flag2.ToLower()}";
+            T2ShortName = team2;
+            T2Result = res2;
         }
         #endregion
 
 
         #region Properties
-
-        public string Flag_img
+        public MainModel Model
         {
-            get { return _flag_img; }
-            set { _flag_img = value; }
+            get { return _model; }
+            set { _model = value; }
         }
 
-        public string Team_short_name
+        public string T1FlagImg
         {
-            get { return _team_short_name; }
-            set { _team_short_name = value; }
+            get { return _t1FlagImg; }
+            set { _t1FlagImg = value; }
         }
 
-        public string Result
+        public string T1ShortName
         {
-            get { return _result; }
-            set { _result = value; }
+            get { return _t1ShortName; }
+            set { _t1ShortName = value; }
+        }
+
+        public string T1Result
+        {
+            get { return _t1Result; }
+            set { _t1Result = value; }
+        }
+
+        public string T2FlagImg
+        {
+            get { return _t2FlagImg; }
+            set { _t2FlagImg = value; }
+        }
+
+        public string T2ShortName
+        {
+            get { return _t2ShortName; }
+            set { _t2ShortName = value; }
+        }
+
+        public string T2Result
+        {
+            get { return _t2Result; }
+            set { _t2Result = value; }
         }
 
         #endregion
