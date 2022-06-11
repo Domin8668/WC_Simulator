ee8e69b4205ea748c91e8bd2ca4f685ac9c1908d
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-06-11 20:57:32 +0200

    Added Model and NavigationStore properties

--- WC_Simulator/ViewModel/BaseClasses/BaseViewModel.cs
+++ WC_Simulator/ViewModel/BaseClasses/BaseViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System.ComponentModel;
+using WC_Simulator.Helpers.Stores;
+using WC_Simulator.Model;
 
 namespace WC_Simulator.ViewModel.BaseClasses
 {
     class BaseViewModel : INotifyPropertyChanged
     {
+        private MainModel _model;
+        private NavigationStore _navigationStore;
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(params string[] namesOfProperties)
@@ -15,6 +19,18 @@ namespace WC_Simulator.ViewModel.BaseClasses
                     PropertyChanged(this, new PropertyChangedEventArgs(prop));
                 }
             }
+        }
+
+        public MainModel Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public NavigationStore NavigationStore
+        {
+            get { return _navigationStore; }
+            set { _navigationStore = value; }
         }
     }
 }
