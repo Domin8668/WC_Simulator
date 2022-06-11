8adc848064dde8df7e2ee5d4d51765945c83d232
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-06-11 19:38:54 +0200

    Added draft password change classes for changing the password from the profile

---
+++ WC_Simulator/ViewModel/ChangePasswordViewModel.cs
@@ -0,0 +1,166 @@
+﻿using WC_Simulator.ViewModel.BaseClasses;
+using WC_Simulator.Model;
+using System;
+using WC_Simulator.DAL.Entities;
+using WC_Simulator.Helpers.Stores;
+using System.Windows.Input;
+using System.Windows;
+using WC_Simulator.Helpers.Hashing;
+
+namespace WC_Simulator.ViewModel
+{
+    internal class ChangePasswordViewModel : BaseViewModel
+    {
+        #region Variables
+
+        private User _currentUser;
+        private MainModel _model;
+        private NavigationStore _navigationStore;
+        private string _username;
+        private string _oldPassword;
+        private string _newPassword;
+        private string _repeatPassword;
+
+        #endregion
+
+
+        #region Constructor
+
+        public ChangePasswordViewModel(MainModel model, NavigationStore navigationStore)
+        {
+            Model = model;
+            _navigationStore = navigationStore;
+            _currentUser = new User();
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public User CurrentUser
+        {
+            get { return _currentUser; }
+            set { _currentUser = value; }
+        }
+
+        public MainModel Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                // test wpisywania loginu:
+                Console.WriteLine($"Nazwa: {Username}");
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string OldPassword
+        {
+            get { return _oldPassword; }
+            set
+            {
+                _oldPassword = value;
+                // test wpisywania hasła:
+                Console.WriteLine($"OldPassword: {OldPassword}");
+                OnPropertyChanged(nameof(OldPassword));
+            }
+        }
+        
+        public string NewPassword
+        {
+            get { return _newPassword; }
+            set
+            {
+                _newPassword = value;
+                // test wpisywania hasła:
+                Console.WriteLine($"NewPassword: {NewPassword}");
+                OnPropertyChanged(nameof(NewPassword));
+            }
+        }
+        
+        public string RepeatPassword
+        {
+            get { return _repeatPassword; }
+            set
+            {
+                _repeatPassword = value;
+                // test wpisywania hasła:
+                Console.WriteLine($"RepeatPassword: {RepeatPassword}");
+                OnPropertyChanged(nameof(RepeatPassword));
+            }
+        }
+
+        
+
+        #endregion
+
+
+        #region Commands
+
+        private ICommand _resetPassword = null;
+
+        public ICommand ResetPassword
+        {
+            get
+            {
+                if (_resetPassword == null)
+                {
+                    _resetPassword = new RelayCommand(arg =>
+                    {
+                        _navigationStore.CurrentViewModel = new ResetPasswordViewModel(Model, _navigationStore);
+                    },
+                    arg => true);
+                }
+                return _resetPassword;
+            }
+        }
+
+        private ICommand _register = null;
+
+        public ICommand Register
+        {
+            get
+            {
+                if (_register == null)
+                {
+                    _register = new RelayCommand(arg =>
+                    {
+                        _navigationStore.CurrentViewModel = new RegisterViewModel(Model, _navigationStore);
+                    },
+                    arg => true);
+                }
+                return _register;
+            }
+        }
+
+
+        // barebone ICommand to copy:
+        //private ICommand _login = null;
+
+
+        //public ICommand Login
+        //{
+        //    get
+        //    {
+        //        if (_login == null)
+        //        {
+        //            _login = new RelayCommand(arg => {
+
+        //            },
+        //            arg => true);
+        //        }
+        //        return _login;
+        //    }
+        //}
+
+        #endregion
+    }
+}
