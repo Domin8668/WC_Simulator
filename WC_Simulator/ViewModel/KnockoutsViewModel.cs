68e2617a85034ebfaa7c644e88f486b0acb143a4
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2022-06-12 18:43:07 +0200

    Added draft KnockoutsViewModel, improved KnockoutsView

---
+++ WC_Simulator/ViewModel/KnockoutsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WC_Simulator.Helpers.Stores;
+using WC_Simulator.Model;
+using WC_Simulator.ViewModel.BaseClasses;
+
+namespace WC_Simulator.ViewModel
+{
+    class KnockoutsViewModel : BaseViewModel
+    {
+        // faza pucharowa mundialu
+        #region Variables
+
+        private MainModel _model;
+        private NavigationStore _navigationStore;
+
+        #endregion
+
+
+        #region Constructor
+
+        public KnockoutsViewModel(MainModel model, NavigationStore navigationStore)
+        {
+            _model = model;
+            _navigationStore = navigationStore;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+
+        #endregion
+
+
+        #region Commands
+
+
+        #endregion
+    }
+}
